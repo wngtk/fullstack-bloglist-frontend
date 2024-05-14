e76f9f1b8d2ecf45f3d6f01c19dc54c85e1c6f29
wngtk <100271394+wngtk@users.noreply.github.com>
2024-05-14 21:18:09 +0800

    Setup project on Windows.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
+import {useEffect, useState} from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import {LoginForm} from "./components/LoginForm.jsx";
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -8,15 +9,16 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+        <LoginForm/>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog}/>
+        )}
     </div>
   )
 }
