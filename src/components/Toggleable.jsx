ab279f13be9879011362a2a27b87c5cd81933b9f
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-16 23:39:04 +0800

    remove unnecessary id

--- src/components/Toggleable.jsx
+++ src/components/Toggleable.jsx
@@ -18,7 +18,7 @@ const Toggleable = forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisiable}>
-        <button onClick={toggleVsible} id="toggle-button">
+        <button onClick={toggleVsible}>
           {props.label}
         </button>
       </div>
