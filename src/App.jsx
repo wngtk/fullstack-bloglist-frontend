6e29f2f4dbaa885236efa77cab2725c102beb553
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-16 16:07:28 +0800

    5.16: Blog List Tests, step 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,49 +4,12 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import './index.css'
 import Toggleable from "./components/Toggleable.jsx";
-import * as PropTypes from "prop-types";
+import BlogForm from "./components/BlogForm.jsx";
 
 function Notification({value}) {
   return <div className="notification">{value}</div>;
 }
 
-function BlogForm({createBlog}) {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const handleTitleChange = ({target}) => { setTitle(target.value) }
-  const handleAuthorChange = ({target}) => { setAuthor(target.value) }
-  const handleUrlChange = ({target}) => { setUrl(target.value) }
-
-  const handleCreate = (event) => {
-    event.preventDefault()
-    createBlog({
-      title, author, url,
-    })
-    console.log("creating blog")
-  }
-
-  return <div>
-    <h2>create new</h2>
-    <form onSubmit={handleCreate}>
-      <p>title:<input type="text" value={title} onChange={handleTitleChange}/></p>
-      <p>author: <input type="text" value={author} onChange={handleAuthorChange}/></p>
-      <p>url: <input type="text" value={url} onChange={handleUrlChange}/></p>
-      <button>create</button>
-    </form>
-  </div>;
-}
-
-BlogForm.propTypes = {
-  onSubmit: PropTypes.func,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  value1: PropTypes.string,
-  onChange1: PropTypes.func,
-  value2: PropTypes.string,
-  onChange2: PropTypes.func
-};
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
---
+++ src/components/BlogForm.jsx
@@ -0,0 +1,42 @@
+import {useState} from "react";
+import * as PropTypes from "prop-types";
+
+function BlogForm({createBlog}) {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleTitleChange = ({target}) => { setTitle(target.value) }
+  const handleAuthorChange = ({target}) => { setAuthor(target.value) }
+  const handleUrlChange = ({target}) => { setUrl(target.value) }
+
+  const handleCreate = (event) => {
+    event.preventDefault()
+    createBlog({
+      title, author, url,
+    })
+    console.log("creating blog")
+  }
+
+  return <div>
+    <h2>create new</h2>
+    <form onSubmit={handleCreate}>
+      <p>title:<input type="text" value={title} onChange={handleTitleChange} placeholder="write blog title here"/></p>
+      <p>author: <input type="text" value={author} onChange={handleAuthorChange}/></p>
+      <p>url: <input type="text" value={url} onChange={handleUrlChange}/></p>
+      <button>create</button>
+    </form>
+  </div>;
+}
+
+BlogForm.propTypes = {
+  onSubmit: PropTypes.func,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  value1: PropTypes.string,
+  onChange1: PropTypes.func,
+  value2: PropTypes.string,
+  onChange2: PropTypes.func
+};
+
+export default BlogForm
